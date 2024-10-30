59d49bcdfc7ad49d79b526bf76f0a1d92258dfe8
dvar98 <varela13guerrero@gmail.com>
2024-10-30 14:29:08 -0500

    uodate

--- parser.py
+++ parser.py
@@ -53,10 +53,12 @@ class Parser:
         """Procesa diferentes tipos de sentencias."""
         if self.current_token.type == "KEYWORD" and self.current_token.value == "def":
             return self.function_def()
-        elif self.current_token.type == "KEYWORD" and self.current_token.value in {"if", "else", "while", "for"}:
+        elif self.current_token.type == "KEYWORD" and self.current_token.value in ("if", "while", "for"):
             return self.control_flow()
         elif self.current_token.type == "KEYWORD" and self.current_token.value == "pass":
             return self.pass_stmt()
+        elif self.current_token.type == "KEYWORD" and self.current_token.value == "return":
+            return self.return_stmt()
         else:
             return self.expression()
 
@@ -160,6 +162,12 @@ class Parser:
         self.match("KEYWORD", "pass")
         return {"type": "pass_stmt"}
 
+    def return_stmt(self):
+        """Regla para `return`."""
+        self.match("KEYWORD", "return")
+        expr = self.expression()
+        return {"type": "return_stmt", "expression": expr}
+
     def match(self, expected_type, expected_value=None):
         """Verifica y consume tokens, lanza error si no coincide."""
         if self.current_token.type == expected_type and (expected_value is None or self.current_token.value == expected_value):
@@ -189,14 +197,62 @@ class Parser:
         return {"type": "block", "statements": statements}
 
     def expression(self):
-        """Manejo básico de expresiones (literals, identifiers)."""
-        if self.current_token.type == "NUMBER":
+        """Manejo de expresiones con operadores."""
+        node = self.term()
+        while self.current_token.type in ("PLUS", "MINUS"):
+            op = self.current_token.type
+            self.advance()
+            right = self.term()
+            node = {"type": "binop", "operator": op, "left": node, "right": right}
+        return node
+
+    def term(self):
+        """Manejo de términos en una expresión."""
+        node = self.factor()
+        while self.current_token.type in ("STAR", "SLASH"):
+            op = self.current_token.type
+            self.advance()
+            right = self.factor()
+            node = {"type": "binop", "operator": op, "left": node, "right": right}
+        return node
+
+    def factor(self):
+        """Manejo de factores dentro de una expresión."""
+        if self.current_token.type == "PLUS":
+            self.advance()
+            return self.factor()
+        elif self.current_token.type == "MINUS":
+            self.advance()
+            return {"type": "unaryop", "operator": "MINUS", "operand": self.factor()}
+        elif self.current_token.type == "NUMBER":
             value = self.current_token.value
             self.advance()
             return {"type": "literal", "value": value}
         elif self.current_token.type == "NAME":
             name = self.current_token.value
             self.advance()
-            return {"type": "identifier", "name": name}
+            if self.current_token.type == "LPAR":
+                # Llamada a función
+                self.match("LPAR")
+                args = self.argument_list()
+                self.match("RPAR")
+                return {"type": "function_call", "name": name, "args": args}
+            else:
+                return {"type": "identifier", "name": name}
+        elif self.current_token.type == "LPAR":
+            self.match("LPAR")
+            node = self.expression()
+            self.match("RPAR")
+            return node
         else:
             self.syntax_error("expression")
+
+    def argument_list(self):
+        """Manejo de listas de argumentos en llamadas a función."""
+        args = []
+        if self.current_token.type != "RPAR":
+            args.append(self.expression())
+            while self.current_token.type == "COMMA":
+                self.match("COMMA")
+                args.append(self.expression())
+        return args
